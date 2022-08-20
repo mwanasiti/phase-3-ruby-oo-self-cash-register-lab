34e1d0707e05770eaa79a0d9dfeb88eeaa339261
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-08-20 16:58:46 +0300

    cash

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+    attr_accessor :total, :discount, :items, :latest
 
+    def initialize(discount= 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity= 1)
+        self.latest = price * quantity
+        @total += self.latest
+        quantity.times do
+            self.items << title
+        end
+    end
+
+    def apply_discount
+        if self.discount != 0
+            percentage = (100.0 - self.discount.to_f) / 100
+            self.total = (self.total * percentage).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        @total -= self.latest
+    end
+end
